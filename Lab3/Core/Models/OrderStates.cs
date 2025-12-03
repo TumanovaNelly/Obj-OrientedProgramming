6f0cd9f1fc591a23733f5454041fdab68a8fdf79
Tumanova Nelly <2-man.off@mail.ru>
2025-12-03 06:11:42 +0300

    [chore]: change order states logic

--- Lab3/Core/Models/OrderStates.cs
+++ Lab3/Core/Models/OrderStates.cs
@@ -6,45 +6,30 @@ namespace Lab3.Core.Models;
 public class AwaitingPaymentState : AOrderState
 {
     public override OrderStatus StatusName => OrderStatus.AwaitingPayment;
-    public override void ProceedToNext(Order order)
-    {
-        order.ChangeState(new CookingState());
-    }
-
-    public override void Cancel(Order order)
-    {
-        order.ChangeState(new CancelledState());
-    }
+    public override void ProceedToNext(Order order) 
+        => order.ChangeState(new CookingState());
+    public override void Cancel(Order order) 
+        => order.ChangeState(new CancelledState());
 }
 
 public class CookingState : AOrderState
 {
     public override OrderStatus StatusName => OrderStatus.Cooking;
-
-    public override void ProceedToNext(Order order)
-    {
-        order.ChangeState(new DeliveryState());
-    }
-
-    public override void Cancel(Order order)
-    {
-        throw new InvalidOperationException("Cannot cancel order, it is already cooking");
-    }
+    public override void ProceedToNext(Order order) => order.ChangeState(new DeliveryState());
+    
+    public override void Cancel(Order order) => 
+        order.ChangeState(new CancelledState());
 }
 
 public class DeliveryState : AOrderState
 {
     public override OrderStatus StatusName => OrderStatus.Delivering;
 
-    public override void ProceedToNext(Order order)
-    {
+    public override void ProceedToNext(Order order) =>
         order.ChangeState(new CompletedState());
-    }
 
-    public override void Cancel(Order order)
-    {
-        throw new InvalidOperationException("Cannot cancel order, it's already delivering");
-    }
+    public override void Cancel(Order order) => 
+        order.ChangeState(new CancelledState());
 }
 
 public class CompletedState : AOrderState
